6f772ada44865813b09d10741ef2c90331ea26d1
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2012-09-17 16:48:11 -0400

    add home html

--- pho/base.php
+++ pho/base.php
@@ -25,11 +25,15 @@ class Pho{
   }
 
   public function send_to_controller($uri){
-    $uri = $this->split_uri($uri);
+    if($uri != "/"){
+      $uri = $this->split_uri($uri);
+    }
     if($this->uri_is_a_resource($uri)){
       $controller = $uri[1] . "_controller";
       if($uri[2]==NULL){$uri[2]="index";}
       $controller::respond_to_request($uri[2]);
+    }else if($uri == "/"){
+      include(dirname(__FILE__)."/../app/views/home.html");
     }else{
       Pho::not_found();
     }
