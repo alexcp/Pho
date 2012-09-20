6fbc47a544f8de5e3ee350797dadfd556aef25b7
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2012-09-20 13:12:43 -0400

    remove unused test

--- pho/controllerTest.php
+++
@@ -1,8 +0,0 @@
-<?
-  class ControllerTest extends PHPUnit_Framework_TestCase{
-
-    public function test_request_is_a_default_function(){
-      $this->assertTrue(Controller::request_is_an_availaible_function("index"));
-    }
-  }
-?>
